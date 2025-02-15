ac44e55f27e1ecc79c45513a4c7bc36bf15c070e
Adarsh-890 <adarshtiwari8912@gmail.com>
2025-02-15 15:49:39 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template  
 
-application = Flask(__name__)  # "app" ki jagah "application" likho
+application = Flask(__name__)
 
 @application.route('/')
 def home():
-    return "Hello, Flask app is running on Render!"
+    return render_template('index.html')  # Ensure index.html exists
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port=5000)
