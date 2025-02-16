26fd021cbfbd85506c9c804af17804d6e98d0173
Adarsh-890 <adarshtiwari8912@gmail.com>
2025-02-16 13:42:00 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-       content: ["./src/**/*.{html,js,jsx}"],
-       theme: {
-         extend: {},
-       },
-       plugins: [],
-}
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
+};
